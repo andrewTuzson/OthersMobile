c3b3a750cc6ad5529c91272c856ff4122410d35f
Andrew Tuzson <andrew@evolempirecreative.com>
2018-02-04 05:54:05 -0600

    - Added logic to disable Create Account button unless all fields are complete - Added profile image to database - Tested that account creation pushes image, username, and email to database and storage

--- OthersMobile/CreateAccountViewController.swift
+++ OthersMobile/CreateAccountViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
 class CreateAccountViewController: UIViewController, UITextFieldDelegate {
     
@@ -16,12 +17,41 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var createAccountButton: UIButton!
+    
+    var selectedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         emailTextField.delegate = self
         usernameTextField.delegate = self
         passwordTextField.delegate = self
+        
+        // Round corners of profile image
+        profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 2
+        profileImageView.clipsToBounds = true
+        
+        // Create tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(CreateAccountViewController.handleSelectProfileImageView))
+        profileImageView.addGestureRecognizer(tapGesture)
+        profileImageView.isUserInteractionEnabled = true
+    }
+    
+    // Disable Create Account button unless all fields are completed
+    func setRequiredFields() {
+        if emailTextField == nil || usernameTextField == nil || passwordTextField == nil || profileImageView.image == #imageLiteral(resourceName: "profileIcon2") {
+            createAccountButton.isEnabled = false
+        } else {
+            createAccountButton.isEnabled = true
+        }
+    }
+    
+    // Handle profile image tap gestures
+    @objc func handleSelectProfileImageView() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     }
 
     // MARK: Return user to Login screen
@@ -54,17 +84,42 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
     @IBAction func createAccountPressed(_ sender: Any) {
         Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user: User?, error: Error?) in
             if error != nil {
-                print(error?.localizedDescription)
+                print(error!.localizedDescription)
                 return
             }
-            let ref = Database.database().reference()
-            let userReference = ref.child("users")
+            
+            // Store profile image in Firebase storage
             let uid = user?.uid
-            let newUserReference = userReference.child(uid!)
-            newUserReference.setValue(["Username" : self.usernameTextField.text!, "Email" : self.emailTextField.text!])
+            let storageRef = Storage.storage().reference(forURL: "gs://others-mobile.appspot.com").child("profile_image").child((user?.uid)!)
+            
+            if let profileImg = self.selectedImage, let imageData = UIImageJPEGRepresentation(profileImg, 0.1) {
+                storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+                    if error != nil {
+                        return
+                    }
+                    
+                    // Push user to database
+                    let profileImageURL = metadata?.downloadURL()?.absoluteString
+                    let ref = Database.database().reference()
+                    let userReference = ref.child("users")
+                    let newUserReference = userReference.child(uid!)
+                    newUserReference.setValue(["Username" : self.usernameTextField.text!, "Email" : self.emailTextField.text!, "profileImageURL" : profileImageURL])
+                })
+            }
         }
     }
     
+}
+
+extension CreateAccountViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            selectedImage = image
+            profileImageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
     
 }
 
